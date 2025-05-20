c21370bcad8d1eb3f5f5e721c72f35eb7a6772d9
Robert Grasfield <120531353+Proton118@users.noreply.github.com>
2025-05-20 10:43:36 -0400

    Moved Some Basic Functionality to PhysicsEngine

--- src/App.java
+++ src/App.java
@@ -9,6 +9,7 @@ public class App extends PApplet {
     private static final float COLLISION_LOSS = 0.25f;
 
     private ArrayList<ICollider> colliders = new ArrayList<>();
+    private PhysicsEngine physicsEngine = new PhysicsEngine();
     private long previousTime = 0;
 
     public static void main(String[] args) {
@@ -26,6 +27,10 @@ public class App extends PApplet {
         colliders.add(new Wall(new Vector(width / 2, height / 2 + 300), 1000, 10));
         colliders.add(new Ball(Color.BLACK, 8, 50, new Vector(width / 2 + 40, height / 2), 5, 3).SetVelocity(new Vector(150, 0)));
 
+        for(ICollider collider : colliders) {
+            physicsEngine.AddCollider(collider);
+        }
+
         previousTime = System.currentTimeMillis();
     }
 
@@ -35,7 +40,7 @@ public class App extends PApplet {
         float deltaTime = (System.currentTimeMillis() - previousTime) / 1000f;
         previousTime = System.currentTimeMillis();
 
-        UpdateColliders(deltaTime);
+        physicsEngine.UpdateColliderPositions(deltaTime);
         CheckCollisions();
         DrawColliders();
     }
@@ -69,11 +74,6 @@ public class App extends PApplet {
         }
     }
 
-    public void UpdateColliders(float deltaTime) {
-        for (ICollider collider : colliders) {
-            collider.Update(deltaTime);
-        }
-    }
     public void CheckCollisions() {
         for (int i = 0; i < colliders.size(); i++) {
             ICollider colliderA = colliders.get(i);
@@ -81,42 +81,9 @@ public class App extends PApplet {
                 ICollider colliderB = colliders.get(j);
                 CollisionData collisionData = colliderA.IsCollidingWith(colliderB);
                 if (collisionData.GetCollided()) {
-                    CheckBallBallCollisions(colliderA, colliderB, collisionData);
+                    physicsEngine.HandleBallCollision((Ball)colliderA, (Ball)colliderB, collisionData);
                 }
             }
-        }
-    }
-
-    public void CheckBallBallCollisions(ICollider colliderA, ICollider colliderB, CollisionData collisionData) {
-        if (colliderA instanceof Ball && colliderB instanceof Ball) {
-            Ball ballA = (Ball) colliderA;
-            Ball ballB = (Ball) colliderB;
-
-            Ball movingBall;
-            Ball stationaryBall;
-
-
-            if(ballA.GetVelocity() > ballB.GetVelocity()){
-                movingBall = ballA;
-                stationaryBall = ballB;
-            } else {
-                movingBall = ballB;
-                stationaryBall = ballA;
-            }
-
-            movingBall.OnCollision(stationaryBall, collisionData.GetPointOfContact());
-            stationaryBall.OnCollision(movingBall, collisionData.GetPointOfContact());
-
-            movingBall.SetVelocity(movingBall.GetVelocity() * (1 - COLLISION_LOSS));
-            float stationaryBallVelocityFinal = (2 * movingBall.GetMass() * movingBall.GetVelocity() + stationaryBall.GetMass() * stationaryBall.GetVelocity()) / (movingBall.GetMass() + stationaryBall.GetMass());
-            System.out.println(stationaryBallVelocityFinal);
-
-            float movingBallVelocityFinal = movingBall.GetVelocity() * (movingBall.GetMass() - stationaryBall.GetMass()) / (movingBall.GetMass() + stationaryBall.GetMass());
-            System.out.println(movingBallVelocityFinal);
-
-            stationaryBall.SetVelocity(stationaryBallVelocityFinal);
-            stationaryBall.SetVelocityDirection(movingBall.GetVelocityDirection());
-            movingBall.SetVelocity(movingBallVelocityFinal);
         }
     }
 }
--- src/PhysicsEngine.java
+++ src/PhysicsEngine.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 
+import Utility.CollisionData;
+
 public class PhysicsEngine {
+    private static final float COLLISION_LOSS = 0.25f;
+
     private ArrayList<ICollider> colliders;
 
     public PhysicsEngine() {
@@ -10,5 +14,47 @@ public class PhysicsEngine {
     public PhysicsEngine AddCollider(ICollider collider) {
         colliders.add(collider);
         return this;
+    }
+
+    public void UpdateColliderPositions(float deltaTime) {
+        for (ICollider collider : colliders) {
+            collider.Update(deltaTime);
+        }
+    }
+
+    public void HandleCollisions() {
+        for (int i = 0; i < colliders.size(); i++) {
+            ICollider colliderA = colliders.get(i);
+            for (int j = i + 1; j < colliders.size(); j++) {
+                ICollider colliderB = colliders.get(j);
+            }
+        }
+    }
+
+    public void HandleBallCollision(Ball ballA, Ball ballB, CollisionData collisionData) {
+        Ball movingBall;
+        Ball stationaryBall;
+
+        if(ballA.GetVelocity() > ballB.GetVelocity()){
+            movingBall = ballA;
+            stationaryBall = ballB;
+        } else {
+            movingBall = ballB;
+            stationaryBall = ballA;
+        }
+
+        movingBall.OnCollision(stationaryBall, collisionData.GetPointOfContact());
+        stationaryBall.OnCollision(movingBall, collisionData.GetPointOfContact());
+
+        movingBall.SetVelocity(movingBall.GetVelocity() * (1 - COLLISION_LOSS));
+        float stationaryBallVelocityFinal = (2 * movingBall.GetMass() * movingBall.GetVelocity() + stationaryBall.GetMass() * stationaryBall.GetVelocity()) / (movingBall.GetMass() + stationaryBall.GetMass());
+        System.out.println(stationaryBallVelocityFinal);
+
+        float movingBallVelocityFinal = movingBall.GetVelocity() * (movingBall.GetMass() - stationaryBall.GetMass()) / (movingBall.GetMass() + stationaryBall.GetMass());
+        System.out.println(movingBallVelocityFinal);
+
+        stationaryBall.SetVelocity(stationaryBallVelocityFinal);
+        stationaryBall.SetVelocityDirection(movingBall.GetVelocityDirection());
+        movingBall.SetVelocity(movingBallVelocityFinal);
     }
 }
